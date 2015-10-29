2f825abff59e40ec814380a3eb4481dabc991a27
wangpandeng <wangpandeng1223@163.com>
2015-10-29 16:20:16 +0800

    test~

---
+++ WishCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WishCard
+//
+//  Created by WangPandeng on 15/10/29.
+//  Copyright © 2015年 GuoguangGaoTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

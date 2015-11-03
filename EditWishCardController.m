9ac0572c367851772834209283c4c96ba9b013bd
wangpandeng <wangpandeng1223@163.com>
2015-11-03 15:56:37 +0800

    上传

--- EditWishCardController.m
+++ EditWishCardController.m
@@ -15,7 +15,7 @@
 //#import "TFHpple.h"
 //#import "TFHppleElement.h"
 
-
+#define krgb(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f]
 @interface EditWishCardController ()<UIScrollViewDelegate>
 {
     NSMutableArray *dataArray;  //容量大数组
@@ -29,7 +29,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+    self.view.backgroundColor =[UIColor whiteColor];
+    [self colorFormString:nil];
     dataArray =[[NSMutableArray alloc] init];
     [self addimage:[UIImage imageNamed:@"back-icon"] title:nil selector:@selector(backClick) location:YES];
     [self addTiTle:self.name];
@@ -104,34 +105,21 @@
         
     }];
 }
--(void)jiexiHtml{
-//[UIColor colorWithCGColor:<#(nonnull CGColorRef)#>:<#(nonnull CIColor *)#>]
-    NSString *str =@"<font color=\"#ffffff\"><span style=\"line-height: 13px;\"><font size=\"5\">为了</font><font size=\"2\">寻你，我错过了许多的良辰美景，错过了闲看花开花落的心情，可我不后悔。因为你是我今生最美的遇见；寻得你，我就拥有了全世界的花开颜色。你的到来，为我拂去了浪迹天涯的孤独，我漂泊的灵魂再也不用辗转流连于亭台楼榭之间，湄湄云水之上。</font>\n</span>\n</font>";
-//    TFHpple *xpathParser = [[TFHpple alloc] initWithHTMLData:[str dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    UIWebView *webView =[[UIWebView alloc] initWithFrame:CGRectMake(10, 10, 100, 100)];
-    [webView loadHTMLString:str baseURL:nil];
-    [self.view addSubview:webView];
-    
-}
 -(void)makeUI{
     //      展示模板的scrollView
-    wishScrollView =[[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, wid, heigh-64-49)];
+    wishScrollView =[[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, wid, heigh-64-40)];
     [self.view addSubview:wishScrollView];
     wishScrollView.pagingEnabled = YES;
     wishScrollView.delegate =self;
     wishScrollView.showsHorizontalScrollIndicator = NO;
     wishScrollView.contentSize = CGSizeMake(dataArray.count*wid, 0);
-
-//     解析html
-    [self jiexiHtml];
     if (dataArray.count>0) {
         for (NSInteger i=0; i<dataArray.count; i++) {
             EditWishCardBean *model = dataArray[i];
 //            EditWishCardView *view = [[EditWishCardView alloc]initWithFrame:CGRectMake(40+i*wid, 40, wid-80, heigh-180)];
 //            [wishScrollView addSubview:view];
             
-            UIImageView *bgImageView = [[UIImageView alloc] initWithFrame:CGRectMake(wid*i, 0, wid, heigh-64-59)];
+            UIImageView *bgImageView = [[UIImageView alloc] initWithFrame:CGRectMake(wid*i, 0, wid, heigh-64-40)];
             [wishScrollView addSubview:bgImageView];
             for (NSInteger i=0; i<model.modelDataArray.count; i++) {
                 EditWishCardModel *editModel =model.modelDataArray[i];
@@ -142,7 +130,10 @@
                     NSLog(@"left:%ld,top:%ld",[self getIntgerFromNSstringWithString:editModel.css_left],[self getIntgerFromNSstringWithString:editModel.css_top]);
                     UILabel *label =[[UILabel alloc] init];
                     label.frame = CGRectMake([self getIntgerFromNSstringWithString:editModel.css_left], [self getIntgerFromNSstringWithString:editModel.css_top], [editModel.css_width integerValue], [editModel.css_height integerValue]);
-                    label.textColor = [UIColor redColor];
+                    if (editModel.css_backgroundColor.length>=10) {
+                        NSArray *arr =[self colorFormString:editModel.css_backgroundColor];
+                        label.backgroundColor = RGBACOLOR([arr[0] integerValue], [arr[1] integerValue], [arr[2] integerValue],[arr[3] floatValue]);
+                    }
                     NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[editModel.conntent dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
                     label.attributedText = attrStr;
                     label.numberOfLines=0;
@@ -170,7 +161,7 @@
 
         }
 
-        pagecontrol = [[UIPageControl alloc] initWithFrame:CGRectMake(wid/2-50, heigh-50, 100, 10)];
+        pagecontrol = [[UIPageControl alloc] initWithFrame:CGRectMake(wid/2-50, heigh-40, 100, 10)];
         pagecontrol.numberOfPages = dataArray.count;
         pagecontrol.pageIndicatorTintColor =[UIColor lightGrayColor];
         pagecontrol.currentPageIndicatorTintColor = [UIColor orangeColor];
@@ -179,7 +170,7 @@
 
     }
    //       底下的View
-    UIView *botView =[[UIView alloc] initWithFrame:CGRectMake(0, heigh-40, wid, 40)];
+    UIView *botView =[[UIView alloc] initWithFrame:CGRectMake(0, heigh-30, wid, 30)];
     UIButton *backgroundbutton =[UIButton buttonWithType:UIButtonTypeCustom];
     backgroundbutton.frame =CGRectMake(0, 0, wid/2-0.5, 40);
     backgroundbutton.backgroundColor=[UIColor grayColor];
@@ -187,7 +178,7 @@
     [backgroundbutton addTarget:self action:@selector(backgroundButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [botView addSubview:backgroundbutton];
     UIButton *musicButton =[UIButton buttonWithType:UIButtonTypeCustom];
-    musicButton.frame =CGRectMake(wid/2, 0, wid/2, 40);
+    musicButton.frame =CGRectMake(wid/2, 0, wid/2, 30);
     musicButton.backgroundColor=[UIColor grayColor];
     [musicButton setTitle:@"音乐" forState:UIControlStateNormal];
     [musicButton addTarget:self action:@selector(musicButtonClick) forControlEvents:UIControlEventTouchUpInside];
@@ -196,22 +187,34 @@
     [self.view addSubview:botView];
 }
 -( NSInteger)getIntgerFromNSstringWithString:(NSString *)string{
-    if ([self isPureInt:string]) {
-        return [string integerValue];
+//    if ([self isPureInt:string]) {
+//        return [string integerValue];
+//    }
+    NSString *sss =[NSString stringWithFormat:@"%@",string];
+    if ([sss rangeOfString:@"p"].location==NSNotFound){
+        return [sss integerValue];
     }
-//    NSString *sss =[NSString stringWithFormat:@"%@",string];
-//    NSString *str =[sss stringByAppendingString:@"px"];
-    NSArray *array = [string componentsSeparatedByString:@"p"];
+    NSArray *array = [sss componentsSeparatedByString:@"p"];
     NSString *lastString =array[0];
-    
+    NSLog(@"lastString:%@",lastString);
     return [lastString integerValue];
+//    NSString *str =[sss stringByAppendingString:@"px"];
+//    NSArray *array = [string componentsSeparatedByString:@"p"];
+//    NSString *lastString =array[0];
+//    
+//    return [lastString integerValue];
 }
-//   判断数字
-- (BOOL)isPureInt:(id )string
-{
-    NSScanner* scan = [NSScanner scannerWithString:string];
-    int val;
-    return [scan scanInt:&val] && [scan isAtEnd];
+////   判断RGB
+-(NSArray *)colorFormString:(NSString *)string{
+    if (string.length>=8) {
+        NSRange range1 = [string rangeOfString:@"("];
+        NSRange range2 = [string rangeOfString:@")"];
+        NSString * strings = [string substringWithRange:NSMakeRange(range1.location+1,range2.location-range1.location-1)];
+        NSArray *array = [strings componentsSeparatedByString:@","];
+
+        return array;
+    }
+    return nil;
 }
 #pragma mark - Click事件
 -(void)backClick{
